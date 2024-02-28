fbf06e4a961fec195ced2436bd55f4d9aea6cef8
Rafael Franca Scofield Lauar <rafavasco12@hotmail.com>
2024-02-27 23:05:45 -0300

    Criando Spawn dos inimigos

--- Assets/_Game/Scripts/Background.cs
+++ Assets/_Game/Scripts/Background.cs
@@ -12,7 +12,7 @@ public class Background : MonoBehaviour
 
         float width = sprite.bounds.size.x;
         float height = sprite.bounds.size.y;
-        float heightCamera = Camera.main.orthographicSize * 2.2f;
+        float heightCamera = Camera.main.orthographicSize * 2.0f;
         float widthWorld = heightCamera / Screen.height * Screen.width;
 
         scaleTemp.x = widthWorld / width;
